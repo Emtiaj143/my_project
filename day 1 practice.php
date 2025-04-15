a9f3f602bbaa5c9d910dd102fad0ca9b3454448c
Emtiaj143 <emtiaj669@gmail.com>
2025-04-15 12:23:02 +0300

    Add files via upload

---
+++ day 1 practice.php
@@ -0,0 +1,16 @@
+<?php
+$name = "Emtiaj";
+$date = date("Y-m-d");
+$course = "PHP Basics";
+?>
+<!DOCTYPE html>
+<head>
+<title>Day-1 PHP practice</title>
+</head>
+<body>
+<h1><?php echo "MY first php page"; ?> </h1>
+<p><?php echo "Hello, my name is $name." ?> <p>
+<p> <?php echo "Todays date is $date." ?> <p>
+<p> <?php echo "I am learning $course. " ?><p>
+</body>
+<?html>
